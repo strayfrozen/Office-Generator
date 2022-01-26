22e8032b39073e91c7779fd41d3609cf68c7e7dd
Stray <strayfrozen@yahoo.com>
2022-01-26 15:44:05 -0600

    set up jest in package.json

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+
 /*GIVEN a command-line application that accepts user input
 WHEN I am prompted for my team members and their information
 THEN an HTML file is generated that displays a nicely formatted team roster based on user input
